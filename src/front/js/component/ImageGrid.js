742cbec2ca5cbc43e17ec7e905ee7d429ac6970b
Angel Cuevas <163451592+AngelCS94@users.noreply.github.com>
2024-09-06 09:53:30 +0000

    terminado

--- src/front/js/component/ImageGrid.js
+++ src/front/js/component/ImageGrid.js
@@ -3,22 +3,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const ImageGrid = () => {
+  const imageStyle = {
+    height: '250px',  // Tamaño uniforme para las imágenes
+    width: '100%',
+    objectFit: 'cover'  // Ajuste para que la imagen no se deforme
+  };
+
   return (
     <Container className="my-4">
       <Row>
         <Col xs={12} md={4}>
-          <div className="image-placeholder"> {/* Aquí irían las imágenes */}
-            Imagen 1
-          </div>
+          {/* Imagen 1 */}
+          <img
+            src="https://images.pexels.com/photos/7518759/pexels-photo-7518759.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Imagen 1"
+            className="img-fluid rounded shadow-sm"
+            style={imageStyle}
+          />
         </Col>
         <Col xs={12} md={4}>
-          <div className="image-placeholder">Imagen 2</div>
+          {/* Imagen 2 */}
+          <img
+            src="https://images.pexels.com/photos/28179155/pexels-photo-28179155/free-photo-of-moda-gente-arte-relajacion.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Imagen 2"
+            className="img-fluid rounded shadow-sm"
+            style={imageStyle}
+          />
         </Col>
         <Col xs={12} md={4}>
-          <div className="image-placeholder">Imagen 3</div>
+          {/* Imagen 3 */}
+          <img
+            src="https://images.pexels.com/photos/27405444/pexels-photo-27405444/free-photo-of-ligero-moda-relajacion-vintage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Imagen 3"
+            className="img-fluid rounded shadow-sm"
+            style={imageStyle}
+          />
         </Col>
       </Row>
-      <Row className="mt-4">
+      {/* <Row className="mt-4">
         <Col xs={12} md={4}>
           <div className="image-placeholder">Imagen 4</div>
         </Col>
@@ -28,7 +50,7 @@ const ImageGrid = () => {
         <Col xs={12} md={4}>
           <div className="image-placeholder">Imagen 6</div>
         </Col>
-      </Row>
+      </Row> */}
     </Container>
   );
 };
