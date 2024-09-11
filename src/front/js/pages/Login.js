1954d623f6a1643e78eac87d63cacf0bdef5424f
BernardoNurvia <167910567+BernardoNurvia@users.noreply.github.com>
2024-09-11 17:36:55 +0000

    actualizacion de archivos

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../styles/footer.css";
+import { Context } from "../store/appContext";
 
 
 export const Login = () => {
+    const { store, actions } = useContext(Context);
+    const [emal, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSumit = (e) =>{
+        e.preventDefault();
+
+        actions.getMessage()
+        
+    }
+
+
     return (
         <div className='bodyPage '>
             <div className='bodyCard'>
@@ -11,12 +24,14 @@ export const Login = () => {
                 <form className='' style={{ width: "25rem" }}>
                     <div className="mb-3">
                         <label for="emailLogin" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="emailLogin" aria-describedby="emailHelp" />
+                        <input type="email" className="form-control" id="emailLogin" aria-describedby="emailHelp"  value={email} 
+                            onChange={(e) => setEmail(e.target.value)}  />
                         
                     </div>
                     <div className="mb-3">
                         <label for="passwordLogin" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="passwordLogin" />
+                        <input type="password" className="form-control" id="passwordLogin"  value={password} 
+                            onChange={(e) => setPassword(e.target.value)}  />
                     </div>
                     <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="rememberMeCheck" />
