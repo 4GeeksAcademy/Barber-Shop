ae40af57a8495d41626df610b4ba8643edf1ac16
Albertrivino <155556342+Albertrivino@users.noreply.github.com>
2024-09-16 18:51:24 +0000

    Created fetch_services and logical and others files

--- src/front/js/component/BookAppointment_Services.js
+++ src/front/js/component/BookAppointment_Services.js
@@ -1,30 +1,28 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 import { Context } from '../store/appContext'; // Importar el contexto
-
-const services = [
-    { id: 1, name: 'Haircut - Premier Stylist', price: 15.00, duration: '1h' },
-    { id: 2, name: 'Haircut - Top Stylist', price: 18.00, duration: '1,25h' },
-    { id: 3, name: 'Salon Director Cut', price: 20.00, duration: '1,5h' },
-    { id: 4, name: 'Vurve Director Cut', price: 17.00, duration: '1h' },
-    { id: 5, name: 'Hair Trim', price: 14.00, duration: '1h' },
-    { id: 6, name: 'Kids Cut (Below 5 years)', price: 11.00, duration: '0,5h' }
-];
+import SummaryCard from './summaryCard';
 
 const BookAppointment_Services = () => {
-  const [selectedService, setSelectedService] = useState(null);
   const navigate = useNavigate();
   const { store, actions } = useContext(Context); // Obtener el contexto
 
+useEffect(()=>{
+actions.getServices()
+},[]) 
+
   const handleContinue = () => {
     if (selectedService) {
-      actions.selectService(services.find(service => service.id === selectedService)); // Guardar el servicio en el store
+      actions.selectService(store.services.find(service => service.id === selectedService)); // Guardar el servicio en el store
       navigate('/book-appointment-date');
     } else {
       alert("Please select a service before continuing.");
     }
   };
+  const selectedProfessional = store.selectedProfessional;
+   
+  const selectedService= store.selectedService;
 
   return (
     <div className="container mt-5" style={{ paddingBottom: '80px' }}>
@@ -33,9 +31,9 @@ const BookAppointment_Services = () => {
           <h3>Step 2 of 3</h3>
           <h2>Select Services</h2>
           <ul className="list-group">
-            {services.map(service => (
+            {store.services.map((service, index) => (
               <li
-                key={service.id}
+                key={index}
                 className="list-group-item d-flex justify-content-between align-items-center"
                 style={{
                   cursor: 'pointer',
@@ -43,23 +41,19 @@ const BookAppointment_Services = () => {
                   border: selectedService === service.id ? '1px solid #FFD700' : '',
                   fontSize: '1.2rem'
                 }}
-                onClick={() => setSelectedService(service.id)}
+                onClick={() => actions.selectService(service)}
               >
                 <div className="d-flex flex-column">
                   <div className="d-flex align-items-center">
-                    {selectedService === service.id && (
+                    {selectedService && selectedService.id === service.id && (
                       <i className="fa-solid fa-circle-check me-2" style={{ color: '#FFD700', fontSize: '1.5rem' }}></i>
                     )}
-                    {service.name}
+                    {service.service_name}
                   </div>
-                  <div className="d-flex align-items-center mt-1" style={{ fontSize: '0.9rem' }}>
-                    <i className="fa-solid fa-clock-rotate-left me-2" style={{ color: '#6c757d', fontSize: '1rem' }}></i>
-                    {service.duration}
                   </div>
-                </div>
                 <div className="d-flex align-items-center">
                   <span className="badge bg-secondary me-3">
-                    EUR {service.price.toFixed(2)}
+                    EUR {service.price}
                   </span>
                 </div>
               </li>
@@ -67,48 +61,16 @@ const BookAppointment_Services = () => {
           </ul>
         </div>
 
-        <div className="col-md-4">
-          <div className="card" style={{ backgroundColor: '#F0F0F0' }}>
-            <div
-              style={{
-                backgroundColor: '#E0E0E0',
-                padding: '20px',
-                display: 'flex',
-                justifyContent: 'center'
-              }}
-            >
-              <img
-                src="https://images.unsplash.com/photo-1599351431613-18ef1fdd27e1?q=80&w=3988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                className="card-img-top"
-                alt="Location"
-                style={{
-                  width: '100%',
-                  height: '200px',
-                  objectFit: 'cover',
-                  objectPosition: 'center'
-                }}
-              />
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Vurve - Bangalore</h5>
-              <p className="card-text">MG Road, Bangalore</p>
-              {store.selectedProfessional && (
-                <div>
-                  <p><strong>Professional: {store.selectedProfessional.name}</strong></p>
-                  <p>{store.selectedProfessional.hours}</p>
-                </div>
-              )}
-              {selectedService && (
-                <div>
-                  <p><strong>{services.find(service => service.id === selectedService).name}</strong></p>
-                  <p>EUR {services.find(service => service.id === selectedService).price.toFixed(2)}</p>
-                </div>
-              )}
-              <button className="btn btn-warning w-100 mb-2" onClick={handleContinue}>Continue</button>
-              <button className="btn btn-secondary w-100" onClick={() => navigate('/book-appointment-proffesional')}>Back</button>
-            </div>
-          </div>
-        </div>
+
+<SummaryCard
+  profeName={selectedProfessional ? selectedProfessional.name : ''}
+  profeLastName={selectedProfessional ? selectedProfessional.last_name : ''}
+  serviName={selectedService ? selectedService.service_name : ''}
+  serviPrice={selectedService ? selectedService.price : ''}
+  handleContinue={handleContinue}
+   backRoute='/book-appointment-proffesional'
+/>
+
       </div>
     </div>
   );
--- src/front/js/component/summaryCard.js
+++ src/front/js/component/summaryCard.js
@@ -1,49 +1,55 @@
 import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { Context } from '../store/appContext';
 
 
-const SummaryCard = ({profeName, profeLastName, services, time, date, handleContinue}) => {
-    const navigate = useNavigate(); // Usamos el hook useNavigate
+const SummaryCard = ({ profeName, profeLastName, serviName, serviPrice, time, date, handleContinue, backRoute }) => {
+  const navigate = useNavigate(); // Usamos el hook useNavigate
 
-    return (
-        <div className="col-md-4">
-        <div className="card" style={{ backgroundColor: '#F0F0F0' }}>
-          <div
+  return (
+    <div className="col-md-4">
+      <div className="card" style={{ backgroundColor: '#F0F0F0' }}>
+        <div
+          style={{
+            backgroundColor: '#E0E0E0',
+            padding: '20px',
+            display: 'flex',
+            justifyContent: 'center'
+          }}
+        >
+          <img
+            src="https://images.unsplash.com/photo-1605497788044-5a32c7078486?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="card-img-top"
+            alt="Location"
             style={{
-              backgroundColor: '#E0E0E0',
-              padding: '20px',
-              display: 'flex',
-              justifyContent: 'center'
+              width: '100%',
+              height: '200px',
+              objectFit: 'cover',
+              objectPosition: 'center'
             }}
-          >
-            <img
-              src="https://images.unsplash.com/photo-1605497788044-5a32c7078486?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top"
-              alt="Location"
-              style={{
-                width: '100%',
-                height: '200px',
-                objectFit: 'cover',
-                objectPosition: 'center'
-              }}
-            />
-          </div>
-          <div className="card-body">
-            {/* El nombre del salón es estático */}
-            <h5 className="card-title">Vurve - Bangalore</h5>
-            <p className="card-text">MG Road, Bangalore</p>
-            {profeName && (
+          />
+        </div>
+        <div className="card-body">
+          {/* El nombre del salón es estático */}
+          <h5 className="card-title">Vurve - Bangalore</h5>
+          <p className="card-text">MG Road, Bangalore</p>
+          {profeName && (
             <div>
-              <p><strong>{profeName} {profeLastName}</strong></p>
+              <p><strong>Professional: </strong>{profeName} {profeLastName}</p>
             </div>
-            )}
-            <p><strong>Total:</strong> EUR 0,00</p>
-            <button className="btn btn-warning w-100 mb-2" onClick={handleContinue}>Continue</button> {/* Actualizar onClick */}
-            <button className="btn btn-secondary w-100" onClick={() => navigate('/book-appointment')}>Back</button>
-          </div>
+          )}
+          {serviName && (
+            <div>
+              <p><strong>Service: </strong>{serviName}</p>
+            </div>
+          )}
+          <p><strong>Total:</strong> EUR {serviPrice ? serviPrice : '0,00'}</p>
+          <button className="btn btn-warning w-100 mb-2" onClick={handleContinue}>Continue</button>
+          <button className="btn btn-secondary w-100" onClick={() => navigate(backRoute)}>Back</button>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
-export default SummaryCard
+export default SummaryCard;
