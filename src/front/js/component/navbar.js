fd20825e73dcc00c4f193817e04b931f450f4b30
Angel Cuevas <163451592+AngelCS94@users.noreply.github.com>
2024-09-26 19:39:49 +0200

    Revert "Development final front"

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,102 +1,19 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Context } from '../store/appContext';
 
-
-//este es el navbar definitivo
 export const Navbar = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const { actions, store } = useContext(Context);
-
-  const toggleNavbar = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  const handleLogout = () => {
-    setIsExpanded(!isExpanded);
-    actions.logout()
-  }
-
-  const scrollToSection = (sectionId) => {
-    const section = document.getElementById(sectionId)
-      if (section){
-        section.scrollIntoView({ behavior: "smooth"})
-      }
-      setIsExpanded(false)
-  }
-
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> {/* Agregado fixed-top */}
-      <div className="container">
-        <Link to="/" className="navbar-brand">
-          <img
-            src="https://res.cloudinary.com/drigqgirt/image/upload/v1725545065/z85szc7sniccil8xdega.png"
-            alt="Logo"
-            style={{ width: "50px", height: "50px" }}
-          />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={toggleNavbar}
-          aria-controls="navbarNav"
-          aria-expanded={isExpanded}
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse justify-content-end ${isExpanded ? "show" : ""}`} id="navbarNav">
-          <ul className="navbar-nav">
-
-          {/* Servicios */}
-          <li className="nav-item">
-              <a
-                href="#services"
-                className="nav-link"
-                onClick={(e) => {
-                  e.preventDefault();
-                  scrollToSection("services");
-                }}
-              >
-                Services
-              </a>
-            </li>
-            {/* Contact */}
-            <li className="nav-item">
-              <a
-                href="#contact"
-                className="nav-link"
-                onClick={(e) => {
-                  e.preventDefault();
-                  scrollToSection("contact");
-                }}
-              >
-                Contact
-              </a>
-            </li>
-             <li className="nav-item">
-              <Link to="/dashboard" className="nav-link" onClick={toggleNavbar} >
-                Dashboard
-              </Link>
-            </li>
-
-            {store.auth === true ? (
-              <li className="nav-item">
-                <Link className="nav-link" to="/" onClick={handleLogout}>
-                  Logout
-                </Link>
-              </li>) : (
-              <li className="nav-item">
-                <Link className="nav-link" to="/login" onClick={toggleNavbar}>
-                  Login
-                </Link>
-              </li>
-            )}
-
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				</Link>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
+				</div>
+			</div>
+		</nav>
+	);
 };
